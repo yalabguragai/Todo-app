37e65b81aea35a0af92ad00d25dd5fc6790dddfd
yalabguragai <yalabguragai1234@gmail.com>
2020-06-06 17:08:59 +0545

    css added

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -25,8 +25,9 @@ urlpatterns = [
     path('',include('tasks.urls')),
     path('register/',users_views.register, name='register'),
     path('profile/',users_views.profile, name='profile'),
-    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+   path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view( template_name='users/login.html' ), name='logout'),
+
 ]
 
 if settings.DEBUG:
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render , redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+
 
 def register(request):
     if request.method == "POST":
